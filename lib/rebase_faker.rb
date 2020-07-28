08f8dbbc925f20941b6244b726fff2771b69410a
Icaro Siqueira <icaro.siqueira@rebase.com.br>
2020-07-28 16:59:31 -0300

    add random_email method

--- lib/rebase_faker.rb
+++ lib/rebase_faker.rb
@@ -1,9 +1,15 @@
 class RebaseFaker
+    NAMES = ['Icaro Siqueira', 
+        'Alexandre Cruz', 
+        'Andre Benjamim', 
+        'Joao Vale', 
+        'Allan Siqueira']
     def self.random_name
-        ['Icaro Siqueira', 
-         'Alexandre Cruz', 
-         'Andre Benjamim', 
-         'Joao Vale', 
-         'Allan Siqueira'].sample
+        NAMES.sample
+    end
+
+    def self.random_email
+        name = NAMES.sample
+        "#{name.gsub!(' ', '.').downcase!}@rebase.com.br"
     end
 end
